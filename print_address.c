7cff93ba4bfb856a5514dc3354018ec551b64b23
Tu Vo <37724651+tuvo1106@users.noreply.github.com>
2019-03-24 20:04:54 -0700

    Update print_address.c

--- print_address.c
+++ print_address.c
@@ -11,15 +11,11 @@ int print_address(va_list l, flags_t *f)
 {
 	char *str;
 	unsigned long int p = va_arg(l, unsigned long int);
-
 	register int count = 0;
 
 	(void)f;
-
-	if (!p)
-		return (_puts("(nil)"));
+	if (!p) return (_puts(NIL));
 	str = convert(p, 16, 1);
-	count += _puts("0x");
-	count += _puts(str);
+	count += _puts(HEXA) + _puts(str);
 	return (count);
 }

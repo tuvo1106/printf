9835da63ee8c033773277d4d59a57acaa1e9b79b
Tu Vo <37724651+tuvo1106@users.noreply.github.com>
2019-03-24 20:05:12 -0700

    Update print_alpha.c

--- print_alpha.c
+++ print_alpha.c
@@ -13,9 +13,8 @@ int print_string(va_list l, flags_t *f)
 	char *s = va_arg(l, char *);
 
 	(void)f;
-
 	if (!s)
-		s = "(null)";
+		s = NULL_STRING;
 	return (_puts(s));
 }
 
@@ -29,6 +28,5 @@ int print_string(va_list l, flags_t *f)
 int print_char(va_list l, flags_t *f)
 {
 	(void)f;
-	_putchar(va_arg(l, int));
-	return (1);
+	return _putchar(va_arg(l, int));
 }

15c75d510286ab8cacd68901e257f5c4fb13b3cb
Tu Vo <37724651+tuvo1106@users.noreply.github.com>
2019-03-24 20:07:23 -0700

    Update print_nums.c

--- print_nums.c
+++ print_nums.c
@@ -12,9 +12,9 @@ int print_int(va_list l, flags_t *f)
 	int n = va_arg(l, int);
 	int res = count_digit(n);
 
-	if (f->space == 1 && f->plus == 0 && n >= 0)
+	if (f->space && !f->plus && n >= 0)
 		res += _putchar(' ');
-	if (f->plus == 1 && n >= 0)
+	if (f->plus && n >= 0)
 		res += _putchar('+');
 	if (n <= 0)
 		res++;
@@ -48,13 +48,8 @@ void print_number(int n)
 	unsigned int n1;
 
 	if (n < 0)
-	{
 		_putchar('-');
-		n1 = -n;
-	}
-	else
-		n1 = n;
-
+	n1 = ABS(n);
 	if (n1 / 10)
 		print_number(n1 / 10);
 	_putchar((n1 % 10) + '0');
@@ -69,13 +64,9 @@ void print_number(int n)
 int count_digit(int i)
 {
 	unsigned int d = 0;
-	unsigned int u;
+	unsigned int u = ABS(i);
 
-	if (i < 0)
-		u = i * -1;
-	else
-		u = i;
-	while (u != 0)
+	while (u)
 	{
 		u /= 10;
 		d++;

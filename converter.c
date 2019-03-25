2c08c92699c7b64a622cc6390cb09cc82a1b5606
Tu Vo <37724651+tuvo1106@users.noreply.github.com>
2019-03-24 20:02:43 -0700

    Update converter.c

--- converter.c
+++ converter.c
@@ -17,11 +17,11 @@ char *convert(unsigned long int num, int base, int lowercase)
 		? "0123456789abcdef"
 		: "0123456789ABCDEF";
 	ptr = &buffer[49];
-	*ptr = '\0';
+	*ptr = NUL;
 	do {
 		*--ptr = rep[num % base];
 		num /= base;
-	} while (num != 0);
+	} while (num);
 
 	return (ptr);
 }
